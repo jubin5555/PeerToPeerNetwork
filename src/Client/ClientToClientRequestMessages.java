38d7598a16e803723a3e93405fefb9f646ca1d4f
jubin <antonyjubin20@gmail.com>
2018-11-28 14:43:25 -0500

    Methods to generate messages to send to other clients and maintain uniformity of messages

---
+++ src/Client/ClientToClientRequestMessages.java
@@ -0,0 +1,24 @@
+package Client;
+
+/**
+ * This class is used to return the messages which are send from the client to other clients.
+ * Currently the only method present is GET
+ * StringBuffer is used here because of been thread safe
+ * @author: Jubin Antony Thykattil.
+ * *
+ **/
+public class ClientToClientRequestMessages {
+    public static String getMessage(Integer RFCId, String hostName) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("GET RFC ");
+        sb.append(RFCId+" ");
+        sb.append("P2P-CI/1.0");
+        sb.append(System.getProperty("line.separator"));
+        sb.append("Host: ");
+        sb.append(hostName);
+        sb.append(System.getProperty("line.separator"));
+        sb.append("OS: ");
+        sb.append(System.getProperty("os.name"));
+        return sb.toString();
+    }
+}
