c4ef9ab110e45aca7dea5adffc1eb0114a9d3f65
jubin <antonyjubin20@gmail.com>
2018-11-28 18:03:59 -0500

    A standard peer object to maintain peer properties across codebase

--- src/POJO/Peer.java
+++ src/POJO/Peer.java
@@ -1,4 +1,17 @@
 package POJO;
 
 public class Peer {
+    private String hostName;
+    private Integer uploadPortNumber;
+
+    public Peer(String hostName,Integer uploadPortNumber){
+        this.hostName=hostName;
+        this.uploadPortNumber=uploadPortNumber;
+    }
+    public String getHostName(){
+        return this.hostName;
+    }
+    public Integer getUploadPortNumber(){
+        return this.uploadPortNumber;
+    }
 }
